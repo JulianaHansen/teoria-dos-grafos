1ac34bcc017c1f322f12a47fed1a85b449cb666d
Pedro <pedro.lourenco@estudante.ufjf.br>
2024-08-27 22:16:16 -0300

    Adiciona leitura dos parametros via comando como solicitado no documento

--- Graph-Basis/main.cpp
+++ Graph-Basis/main.cpp
@@ -4,71 +4,23 @@
 
 int main(int argc, char* argv[])
 {
+    if (argc != 5) { 
+        std::cerr << "Uso: " << argv[0] << "<arquivo_entrada> <arquivo_saida> <Op_Direc> <Op_PesoAresta>\n";
+        return 1;
+    }
+
+    // Lê os argumentos da linha de comando
+    const char* entrada = argv[1];
+    const char* saida = argv[2];
+    int direcionado = std::stoi(argv[3]);
+    int ponderado = std::stoi(argv[4]);
+
     int valor = -1;
-    int direcionado = -1; // vai salvar se o grafo é direcionado ou nao para usar essa inf no menu de funcionalidades. inicializado com -1 pra nao dar erro 
-    std::ifstream input_file("instances_example/5nU_2.dat");
-
-    while(direcionado != 0 && direcionado != 1)
-    {
-      std::cerr << "Ola! Voce quer trabalhar com um grafo Nao Direcionado(0)" << std::endl;
-      std::cerr << "ou um grafo Direcionado(1)? " << std::endl; 
-      std::cin >> direcionado;
-     
-    }
-    
-    if (direcionado == 0) //é um grafo nao direcionado entao sao os arquivos U
-    {
-        while(valor != 5 && valor != 15 && valor != 50 && valor != 100)
-        {
-            std::cerr << "Voce quer trabalhar com um grafo Nao Direcionado com quantos nos? 5, 15, 50 ou 100?" << std::endl;
-            std::cin >> valor;
-        }
-        
-        if(valor == 5){
-            input_file.close(); 
-            input_file.open("instances_example/5nU_2.dat");
-        }
-        if(valor == 15){
-            input_file.close(); 
-            input_file.open("instances_example/15nU_2.dat");
-        }
-        if(valor == 50){
-            input_file.close(); 
-            input_file.open("instances_example/50nU_2.dat");
-        }
-        if(valor == 100){
-            input_file.close(); 
-            input_file.open("instances_example/100nU.dat");
-        }
-    }
-    else //é um grafo direcionado entao sao os arquivos D
-    {
-        while(valor != 5 && valor != 15 && valor != 50 && valor != 100)
-        {
-            std::cerr << "Voce quer trabalhar com um grafo Nao Direcionado com quantos nos? 5, 15, 50 ou 100?" << std::endl;
-            std::cin >> valor;
-        }
-
-        if(valor == 5){
-            input_file.close(); 
-            input_file.open("instances_example/5nD_2.dat");
-        }
-        if(valor == 15){
-            input_file.close(); 
-            input_file.open("instances_example/15nD_2.dat");
-        }
-        if(valor == 50){
-            input_file.close(); 
-            input_file.open("instances_example/50nD_2.dat");
-        }
-        if(valor == 100){
-            input_file.close(); 
-            input_file.open("instances_example/100nD.dat");
-        }
-    }
+    std::ifstream arquivoEntrada(entrada);
+    std::ifstream arquivoSaida(saida);
     
     
-    if(!input_file){
+    if(!arquivoEntrada){
          std::cerr << "Erro ao abrir o arquivo de instancia." << std::endl;
          return 1;
     }
@@ -76,26 +28,18 @@ int main(int argc, char* argv[])
      Graph *graph = new Graph();
 
     size_t number_of_nodes;
-    input_file >> number_of_nodes; // Lê o número de nós do arquivo de instância
+    arquivoEntrada >> number_of_nodes; // Lê o número de nós do arquivo de instância
 
-    int ponderado = -1;
-
-    while(ponderado != 0 && ponderado != 1)
-        {
-            std::cerr << "Voce quer trabalhar com um grafo com Arestas Nao Ponderadas(0) ou Ponderadas (1)?" << std::endl;
-            std::cin >> ponderado;
-        }
-
-    if(ponderado == 0){
+    if(ponderado == 0 ){
         for (size_t i = 0; i < number_of_nodes; ++i) {
             size_t node_id1;
             size_t node_id2;  
-            input_file >> node_id1;// Lê o no de saida
+            arquivoEntrada >> node_id1;// Lê o no de saida
             graph->add_node(node_id1); // cria o no
-            input_file >> node_id2; // Lê o no alvo
+            arquivoEntrada >> node_id2; // Lê o no alvo
             graph->add_node(node_id2); // cria o no
             float weight;
-            input_file >> weight; // Lê o edge weight
+            arquivoEntrada >> weight; // Lê o edge weight
             graph->add_edge(node_id1,node_id2,0);
      } 
     }
@@ -103,12 +47,12 @@ int main(int argc, char* argv[])
         for (size_t i = 0; i < number_of_nodes; ++i) {
             size_t node_id1;
             size_t node_id2;  
-            input_file >> node_id1;// Lê o no de saida
+            arquivoEntrada >> node_id1;// Lê o no de saida
             graph->add_node(node_id1); // cria o no
-            input_file >> node_id2; // Lê o no alvo
+            arquivoEntrada >> node_id2; // Lê o no alvo
             graph->add_node(node_id2); // cria o no
             float weight;
-            input_file >> weight; // Lê o edge weight
+            arquivoEntrada >> weight; // Lê o edge weight
             graph->add_edge(node_id1,node_id2,weight);
      } 
     }
@@ -124,7 +68,6 @@ int main(int argc, char* argv[])
         std::cerr << "FUNCIONALIDADES: O que voce quer ver?" << std::endl;
         std::cerr << "Selecione um dos valores abaixo:" << std::endl;
         std::cerr << "(1) Caminho min entre dois vertices (alg Djkstra)       (2) Caminho min entre dois vertices (alg Floyd)" << std::endl;
-        //std::cerr << "(3) Arvore Geradora Minima (alg Prim)                   (4) Arvore Geradora Minima (alg Kruskal)" << std::endl;
         std::cerr << "(3) Arvore em ordem de caminhamento em profundidade     (4) O raio, o diametro, o centro e a periferia do grafo" << std::endl;
         std::cerr << "(5) O conjunto de vertices de articulacao               " << std::endl;
 
