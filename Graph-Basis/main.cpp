913af91841a810e62bed9fc9ab5a8b56fd7d8463
Pedro <pedro.lourenco@estudante.ufjf.br>
2024-09-25 11:48:35 -0300

    Adiciona entrada de parâmetros para o arquivo e algoritimo

--- Graph-Basis/main.cpp
+++ Graph-Basis/main.cpp
@@ -3,21 +3,44 @@
 #include <iostream>
 #include <vector>
 
-int main() {
-    Graph g;
-    std::string nomeArquivo = "MGGPP/n100d03p2i5.txt";
+int main(int argc, char *argv[]) {
 
-    if (g.lerArquivo(nomeArquivo) != 0) {
+    if (argc < 3){
+        std::cout << "Nenhuma instância reconhecida" << std::endl;
         return -1;
-    }
+    } else {
 
-    // Executa o algoritmo guloso para resolver o MGGPP
-    g.algoritmoGulosoMGGPP();
-    g.imprimirSubgrafos();
-    
-    int somaDosGaps = g.calcularSomaDosGaps();
-    if (somaDosGaps != -1) {
-        std::cout << "Soma dos gaps: " << somaDosGaps << std::endl;
+        Graph g;
+        std::string nomeArquivo = argv[1];
+        int alg = std::stoi(argv[2]);
+
+        if (g.lerArquivo(nomeArquivo) != 0) {
+            return -1;
+        }
+
+        switch (alg){
+            // GULOSO
+            case 1:
+                // Executa o algoritmo guloso para resolver o MGGPP
+                g.algoritmoGulosoMGGPP();
+                g.imprimirSubgrafos();
+            break;
+            // GULOSO RANDOMIZADO ADAPTATIVO
+            case 2:
+            break;
+            // GULOSO RANDOMIZADO ADAPTATIVO REATIVO
+            case 3:
+            break;
+            // NENHUMA ALTERNATIVA SELECIONADA
+            default:
+                std::cout << "Nenhum algoritimo selecionado" << std::endl;
+                return -1;
+        }
+        
+        int somaDosGaps = g.calcularSomaDosGaps();
+        if (somaDosGaps != -1) {
+            std::cout << "Soma dos gaps: " << somaDosGaps << std::endl;
+        }
     }
     return 0;
 }
